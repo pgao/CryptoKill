742586f8b1f79fd81578dea083fc52877a133312
Peter Gao <pgao@MAW.(none)>
2013-09-14 18:00:42 -0700

    server

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject app "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"], [ring "0.3.7"]]
+  :dependencies [[org.clojure/clojure "1.3.0"], [ring/ring-jetty-adapter "0.3.7"]]
   :main app.core)
--- src/app/core.clj
+++ src/app/core.clj
@@ -1,63 +1,20 @@
 (ns app.core)
 
-(import (javax.crypto KeyGenerator SecretKey Cipher))
-(import (javax.crypto.spec SecretKeySpec))
-(import (java.io File FileOutputStream DataInputStream FileInputStream))
-(import (java.util Properties))
-(import (org.apache.commons.codec.binary Base64))
- 
-(def msg "Hello encryption world!")
-(defn encode-base64 [raw] (. (new Base64) encode raw))
-(defn decode-base64 [coded] (. (new Base64) decode coded))
- 
-(def aes (. KeyGenerator getInstance "AES"))
-(def cipher (. Cipher getInstance "AES"))
-(def encrypt (. Cipher ENCRYPT_MODE))
-(def decrypt (. Cipher DECRYPT_MODE))
- 
-(defn writekey [rawkey filename]    
-    ( let [out (new FileOutputStream (new File filename))]
-          (do (. out write rawkey)
-              (. out close))))
-     
-(defn readkey [filename]
-    (let [file (new File filename)
-          rawkey (byte-array (. file length))
-          in  (new DataInputStream (new FileInputStream file))]
-          (do (. in readFully rawkey)
-              (. in close)
-              rawkey
-          )))
-           
-(defn get-propfile [filename]  
-    (let [prop (new Properties)]
-        (do (. prop load (new FileInputStream filename)))
-        prop))
-     
- 
-(defn genkey [keygen]
-    (do (. keygen init  128)
-        (. (. keygen generateKey ) getEncoded)
-    )
-)   
-     
-(defn do-encrypt [rawkey plaintext]
-    (let [cipher (. Cipher getInstance "AES")]
-        (do (. cipher init encrypt (new SecretKeySpec rawkey "AES"))
-            (. cipher doFinal (. plaintext getBytes)))))
-     
-(defn do-decrypt [rawkey ciphertext]
-    (let [cipher (. Cipher getInstance "AES")]
-        (do (. cipher init  decrypt (new SecretKeySpec rawkey "AES"))
-            (new String(. cipher doFinal ciphertext)))))
-             
-             
-(defn get-password [key rawkey filename]
-    (let [ props (get-propfile filename)
-           coded (. props getProperty key)
-           cipher (decode-base64 coded)]
-         (do (do-decrypt rawkey cipher))))
+(load "crypto")
+(use '[ring.adapter.jetty :only [run-jetty]])
+
+(defn app [req]
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body "Hello world!"})
 
 (defn -main
-    [& args]
-    (println "HELLO WORLD"))
+  [& args]
+  (defonce server (run-jetty app {:port 8000 :join? false}))
+  (def secretkey "secret")
+  (println "key: " secretkey)
+  (def encrypted (encrypt "MESSAGE" secretkey))
+  (println "encrypted: " encrypted)
+  (def decrypted (decrypt encrypted secretkey))
+  (println "decrypted: " decrypted)
+  (println "HELLO WORLD"))
