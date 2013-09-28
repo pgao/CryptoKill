18737e6e10ce70d3dbe556ddaa509d068119523a
Peter Gao <pgao@MAW.(none)>
2013-09-28 13:15:41 -0700

    blah

--- project.clj
+++ project.clj
@@ -1,4 +1,8 @@
 (defproject app "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"], [ring/ring-jetty-adapter "0.3.7"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+  				 [ring/ring-core "1.2.0"]
+  				 [ring/ring-jetty-adapter "1.2.0"]
+  				 [compojure "1.1.5"]
+  				 [lib-noir "0.6.8"]]
   :main app.core)
--- src/app/core.clj
+++ src/app/core.clj
@@ -1,20 +1,20 @@
-(ns app.core)
+(ns app.core
+  (:use ring.adapter.jetty)
+  (:use compojure.core)
+  (:require [compojure.route :as route])
+  (:load "crypto"))
 
-(load "crypto")
-(use '[ring.adapter.jetty :only [run-jetty]])
-
-(defn app [req]
-  {:status 200
-   :headers {"Content-Type" "text/plain"}
-   :body "Hello world!"})
+(defroutes app
+  (GET "/" [] "OH HELLO THERE")
+  (route/not-found "<h1>Page not found</h1>"))
 
 (defn -main
   [& args]
   (defonce server (run-jetty app {:port 8000 :join? false}))
   (def secretkey "secret")
-  (println "key: " secretkey)
+  (println "key:" secretkey)
   (def encrypted (encrypt "MESSAGE" secretkey))
-  (println "encrypted: " encrypted)
+  (println "encrypted:" encrypted)
   (def decrypted (decrypt encrypted secretkey))
-  (println "decrypted: " decrypted)
+  (println "decrypted:" decrypted)
   (println "HELLO WORLD"))
